56ee6f6c8122b68778b4d7b8ab28df2b048fff26
Armando Basante <armandobasante@fymtech.com>
2024-03-20 10:37:27 -0500

    se agrega la función de listar y borrar todos los archivos *.zip, se elimian los espacios en blanco dentro de la lista lineas

--- appco9.py
+++ appco9.py
@@ -1,30 +1,41 @@
 import subprocess
-import datetime
+import os
 
 lista = []
 
-ruta = "C:\\Users\\T.I\\Documents\\python package"
+ruta = "C:\\Users\\T.I\\Documents\\Proyectos_Automatizacion\\"
 
-#command = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime'
-command_file = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
-command_folder = rf'Get-ChildItem -Path "{ruta}" -Directory -File | Select-Object -Property Name'
-output = subprocess.check_output(["powershell", "-Command", command_file])
+
+#command_file = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
+command_folder = rf'Get-ChildItem -Path "{ruta}" -Directory | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
+output = subprocess.check_output(["powershell", "-Command", command_folder])
 salida= output.decode("utf-8")
 print(salida)
 
-lineas = salida.splitlines()[3:]  # Elimina las dos primeras líneas que contienen encabezados
-print (lineas[-3])
-print (len(lineas) - 3)
-#print (lineas[83])
+lineas = salida.splitlines()[3:-2]  # Elimina los espacios vacios de la lista
+print ("el ultimo archivo creado es: ", lineas[-1])
+print ("numero total de archivos: ",len(lineas))
 
-print("elementos a eliminar", len(lineas) - 5)
-#print (lineas[82])
-print (lineas[-5])
 
-for i in range(len(lineas) - 4):
+print("cantidad de archivos a eliminar: ", len(lineas) - 2)
+print ("se elimina los archivos en ordes hasta: ", lineas[-3])
+
+for i in range(len(lineas) - 2):
     elemento = lineas[i].split()[0]
-    delete = f'Remove-Item -Path "{ruta}{elemento}"'
-    subprocess.run(["powershell.exe", "-Command", delete], check=True)
+    print(elemento)
+    #delete = f'Remove-Item -Path "{ruta}{elemento}" -Recurse'
+    #subprocess.run(["powershell.exe", "-Command", delete], check=True)
+    
+archivos_zip = []
+
+for archivo in os.listdir(ruta):
+    if archivo.endswith(".zip"):
+      archivos_zip.append(archivo)
+      #delete = f'Remove-Item -Path "{ruta}{archivo}"'
+      #subprocess.run(["powershell.exe", "-Command", delete], check=True)
+    
+
+print(archivos_zip)
 
     
 
