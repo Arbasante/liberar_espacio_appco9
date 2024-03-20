1703ddbdc1f5b0c6245bf0599f5b5445ed9bc691
Armando Basante <armandobasante@fymtech.com>
2024-03-19 19:22:17 -0500

    se lista ordenadamente los archivos de una carpeta y se elminan dejando los dos mas recientes

--- appco9.py
+++ appco9.py
@@ -1,11 +1,29 @@
 import subprocess
+import datetime
 
-#ruta = "C:\\Users\\T.I\\Documents\\python package"
+lista = []
 
-command = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime'
+ruta = "C:\\Users\\T.I\\Documents\\python package"
+
+#command = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime'
+command = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
 output = subprocess.check_output(["powershell", "-Command", command])
 salida= output.decode("utf-8")
 print(salida)
 
 lineas = salida.splitlines()[3:]  # Elimina las dos primeras líneas que contienen encabezados
-print (lineas[1])
+print (lineas[-3])
+print (len(lineas) - 3)
+#print (lineas[83])
+
+print("elementos a eliminar", len(lineas) - 5)
+#print (lineas[82])
+print (lineas[-5])
+
+for i in range(len(lineas) - 4):
+    elemento = lineas[i].split()[0]
+    delete = f'Remove-Item -Path "{ruta}{elemento}"'
+    subprocess.run(["powershell.exe", "-Command", delete], check=True)
+
+    
+
