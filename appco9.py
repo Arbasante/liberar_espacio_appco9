fdd3fd7899d46d8194a15c565cbadc32f261cf4d
Armando Basante <armandobasante@fymtech.com>
2024-03-21 17:12:19 -0500

    se crea una funcion que tiene todo el proceso de eliminación: - se pasan una ruta con un bucle para que sea el parametro de entrada de la función - la función borra todos los archivos .zip y si ordena en orden ascendente por fecha de creación - si la cantidad de carpetas es mayor a 2 se eliminan las carpetas con fecha de creación mas antigua

--- appco9.py
+++ appco9.py
@@ -1,41 +1,55 @@
 import subprocess
 import os
 
-lista = []
-
-ruta = "C:\\Users\\T.I\\Documents\\Proyectos_Automatizacion\\"
 
 
-#command_file = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
-command_folder = rf'Get-ChildItem -Path "{ruta}" -Directory | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
-output = subprocess.check_output(["powershell", "-Command", command_folder])
-salida= output.decode("utf-8")
-print(salida)
 
-lineas = salida.splitlines()[3:-2]  # Elimina los espacios vacios de la lista
-print ("el ultimo archivo creado es: ", lineas[-1])
-print ("numero total de archivos: ",len(lineas))
+def liberar_espacio(ruta):
+
+    #command_file = rf'Get-ChildItem -Path "{ruta}" -File | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
+    command_folder = rf'Get-ChildItem -Path "{ruta}" -Directory | Select-Object -Property Name, CreationTime | Sort-Object -Property CreationTime'
+    output = subprocess.check_output(["powershell", "-Command", command_folder])
+    salida= output.decode("utf-8")
+    print(salida)
+
+    lineas = salida.splitlines()[3:-2]  # Elimina los espacios vacios de la lista
+    print ("el ultimo archivo creado es: ", lineas[-1])
 
 
-print("cantidad de archivos a eliminar: ", len(lineas) - 2)
-print ("se elimina los archivos en ordes hasta: ", lineas[-3])
 
-for i in range(len(lineas) - 2):
-    elemento = lineas[i].split()[0]
-    print(elemento)
-    #delete = f'Remove-Item -Path "{ruta}{elemento}" -Recurse'
-    #subprocess.run(["powershell.exe", "-Command", delete], check=True)
+    #print("cantidad de archivos a eliminar: ", len(lineas) - 2)
+    #print ("se elimina los archivos en orden hasta: ", lineas[-3])
+
+    archivos_zip = []
+
+    for archivo in os.listdir(ruta):
+        if archivo.endswith(".zip"):
+            archivos_zip.append(archivo)
+            delete = f'Remove-Item -Path "{ruta}{archivo} -Force"'
+            subprocess.run(["powershell.exe", "-Command", delete], check=True)
+
+    if len(lineas) > 2:
+
+        print ("numero total de archivos: ",len(lineas))
+        for i in range(len(lineas) - 2):
+            elemento = lineas[i].split()[0]
+            print(elemento)
+            delete = f'Remove-Item -Path "{ruta}{elemento}" -Recurse -Force'
+            subprocess.run(["powershell.exe", "-Command", delete], check=True)
+        
+
     
-archivos_zip = []
 
-for archivo in os.listdir(ruta):
-    if archivo.endswith(".zip"):
-      archivos_zip.append(archivo)
-      #delete = f'Remove-Item -Path "{ruta}{archivo}"'
-      #subprocess.run(["powershell.exe", "-Command", delete], check=True)
-    
+        print(archivos_zip)
 
-print(archivos_zip)
+
+if __name__ == '__main__':
+
+
+    path = ["C:\\Users\\T.I\\Documents\\Proyectos_Automatizacion_copia\\"]
+
+    for ruta in path:
+        liberar_espacio(ruta)
 
     
 
